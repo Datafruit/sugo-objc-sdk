14b4192a1beefcbbd6bf08eadcc7872216abbc38
Zack <zackx@foxmail.com>
2017-04-19 19:47:26 +0800

    Update JavaScript

--- Sugo/WebViewExcute.Sugo.js
+++ Sugo/WebViewExcute.Sugo.js
@@ -4,12 +4,14 @@
         sugo.trackStayEvent();
     }
     sugo.bindEvent();
-    window.addEventListener('hashchange', function() {
-        sugo.view_props = {};
-        sugo.init_path();
-        if (sugo.can_track_web_page) {
-            sugo.track('浏览', sugo.view_props);
-            sugo.trackStayEvent();
-        }
-    })
+    if (!window.sugo) {
+        window.addEventListener('hashchange', function() {
+            sugo.view_props = {};
+            sugo.init_path();
+            if (sugo.can_track_web_page) {
+                sugo.track('浏览', sugo.view_props);
+                sugo.trackStayEvent();
+            }
+        })
+    }
     
