8a123c36da580ce49dd6218a12d56e7b042172c8
lzackx <zackx@foxmail.com>
2018-01-04 14:43:24 +0800

    Fix SugoWeexModule.m

--- Sugo/Weex/SugoWeexModule.m
+++ Sugo/Weex/SugoWeexModule.m
@@ -13,47 +13,47 @@
 
 @synthesize weexInstance;
 
-WX_EXPORT_METHOD(@selector(track:props:))
+WX_EXPORT_METHOD_SYNC(@selector(track:props:))
 - (void)track:(NSString *)eventName props:(NSDictionary *)props {
     [[Sugo sharedInstance] trackEvent:eventName properties:props];
 }
 
-WX_EXPORT_METHOD(@selector(timeEvent:))
+WX_EXPORT_METHOD_SYNC(@selector(timeEvent:))
 - (void)timeEvent:(NSString *)eventName {
     [[Sugo sharedInstance] timeEvent:eventName];
 }
 
-WX_EXPORT_METHOD(@selector(registerSuperProperties:))
+WX_EXPORT_METHOD_SYNC(@selector(registerSuperProperties:))
 - (void)registerSuperProperties:(NSDictionary *)superProps {
     [[Sugo sharedInstance] registerSuperProperties:superProps];
 }
 
-WX_EXPORT_METHOD(@selector(registerSuperPropertiesOnce:))
+WX_EXPORT_METHOD_SYNC(@selector(registerSuperPropertiesOnce:))
 - (void)registerSuperPropertiesOnce:(NSDictionary *)superProps {
     [[Sugo sharedInstance] registerSuperPropertiesOnce:superProps];
 }
 
-WX_EXPORT_METHOD(@selector(unregisterSuperProperty:))
+WX_EXPORT_METHOD_SYNC(@selector(unregisterSuperProperty:))
 - (void)unregisterSuperProperty:(NSString *)superPropertyName {
     [[Sugo sharedInstance] unregisterSuperProperty:superPropertyName];
 }
 
-WX_EXPORT_METHOD(@selector(getSuperProperties:))
+WX_EXPORT_METHOD_SYNC(@selector(getSuperProperties:))
 - (void)getSuperProperties:(WXModuleCallback)callback {
     callback([[Sugo sharedInstance] currentSuperProperties]);
 }
 
-WX_EXPORT_METHOD(@selector(clearSuperProperties))
+WX_EXPORT_METHOD_SYNC(@selector(clearSuperProperties))
 - (void)clearSuperProperties {
     [[Sugo sharedInstance] clearSuperProperties];
 }
 
-WX_EXPORT_METHOD(@selector(login:dimension:))
+WX_EXPORT_METHOD_SYNC(@selector(login:dimension:))
 - (void)login:(nullable NSString *)userIdKey dimension:(nullable NSString *)userIdValue {
    [[Sugo sharedInstance] trackFirstLoginWith:userIdValue dimension:userIdKey];
 }
 
-WX_EXPORT_METHOD(@selector(logout))
+WX_EXPORT_METHOD_SYNC(@selector(logout))
 - (void)logout {
    [[Sugo sharedInstance] untrackFirstLogin];
 }
