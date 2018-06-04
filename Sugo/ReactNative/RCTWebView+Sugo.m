7c4377e07ada3b4d77c337c24e376da9319f9f17
lzackx <lzackx@lzackx.com>
2018-06-04 11:35:38 +0800

    Fix compile error

---
+++ Sugo/ReactNative/RCTWebView+Sugo.m
@@ -0,0 +1,16 @@
+//
+//  RCTWebView+Sugo.m
+//  Pods-SugoRN
+//
+//  Created by lzackx on 2018/5/30.
+//
+
+#import <UIKit/UIKit.h>
+#import <React/RCTWebView.h>
+
+@implementation RCTWebView (Sugo)
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+}
+
+@end
---
+++ Sugo/ReactNative/SugoRNModule.h
@@ -0,0 +1,14 @@
+//
+//  SugoRNModule.h
+//  sugo-objc-sdk
+//
+//  Created by lzackx on 2018/1/2.
+//  Copyright © 2018年 facebook. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+
+@interface SugoRNModule : NSObject<RCTBridgeModule>
+
+@end
